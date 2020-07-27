bff97b8d2bd790641093dcc5ea89acb37fa7e0bd
Kit Jonathan Te <kjonathante@gmail.com>
2020-07-27 11:48:30 -0700

    Fixed isEmailAlreadyExist input validation.

--- src/feature/user/register/isEmailAlreadyExist.ts
+++ src/feature/user/register/isEmailAlreadyExist.ts
@@ -5,20 +5,23 @@ import {
   ValidatorConstraintInterface,
 } from 'class-validator'
 
-import { User } from '../../../entity/User'
+import { UserModel } from '../../../entity/User'
 
 @ValidatorConstraint({ async: true })
 export class IsEmailAlreadyExistConstraint
   implements ValidatorConstraintInterface {
-  validate(email: string) {
-    return User.findOne({ where: { email } }).then((user) => {
+  validate(email: string): any {
+    return UserModel.findOne({ email }).then((user) => {
+      // console.log('Inside IsEmailAlreadyExistConstraint. user=', user)
       if (user) return false
       return true
     })
   }
 }
 
-export function IsEmailAlreadyExist(validationOptions?: ValidationOptions) {
+export function IsEmailAlreadyExist(
+  validationOptions?: ValidationOptions
+): any {
   return function (object: Record<string, any>, propertyName: string) {
     registerDecorator({
       target: object.constructor,
