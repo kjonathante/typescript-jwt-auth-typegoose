77ffb593056e9f6c5c9f76fae7dbe863c27b56b7
Kit Jonathan Te <kjonathante@gmail.com>
2020-07-28 17:39:59 -0700

    Added recipes query. Used populate instead of fieldresolver for author which saved a lot of trip back to mongodb.

--- src/feature/recipe/RecipeResolver.ts
+++ src/feature/recipe/RecipeResolver.ts
@@ -3,33 +3,40 @@ import {
   Mutation,
   Arg,
   Ctx,
-  Root,
-  FieldResolver,
+  // Root,
+  // FieldResolver,
   UseMiddleware,
+  Query,
 } from 'type-graphql'
 import { AppContext } from '../../type/AppContext'
 import { RecipeModel, Recipe } from '../../entity/Recipe'
-import { UserModel, User } from '../../entity/User'
+// import { UserModel, User } from '../../entity/User'
 import { isAuth } from '../user/isAuth'
 
 @Resolver(() => Recipe)
 export class RecipeResolver {
+  @Query(() => [Recipe])
+  async recipes(): Promise<Recipe[]> {
+    return await RecipeModel.find({}).populate('author')
+  }
+
   @Mutation(() => Recipe)
   @UseMiddleware(isAuth)
   async addRecipe(
     @Arg('title') title: string,
     @Ctx() { payload }: AppContext
   ): Promise<Recipe> {
-    // console.log('Inside addRecipe. payload=', payload)
     const recipe = await RecipeModel.create({ title, author: payload.userId })
-    // console.log('Inside addRecipe. recipe=\n', recipe)
-    return recipe
+
+    // return recipe
+    // or
+    return await RecipeModel.populate(recipe, { path: 'author' })
   }
 
-  @FieldResolver()
-  // async author(@Root() recipe: Recipe): Promise<User> {
-  async author(@Root() recipe: any): Promise<User> {
-    // console.log('Inside FieldResolver')
-    return await UserModel.findById(recipe.author)
-  }
+  // @FieldResolver()
+  // // async author(@Root() recipe: Recipe): Promise<User> {
+  // async author(@Root() recipe: any): Promise<User> {
+  //   // console.log('Inside FieldResolver')
+  //   return await UserModel.findById(recipe.author)
+  // }
 }
